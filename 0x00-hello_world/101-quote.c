14d143d73cd27aa8826b0de97af3d2dfbe48dde1
1rubiz <1izekorruby@gmail.com>
2022-04-07 17:34:44 -0700

    advanced

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 
 /*
- * main- print out a certain string followed by anew line.
+ * main-prints "and that piece of art is useful\" - Dora Korpar, 2015-10-19" followed by anew line.
  * Return: 0 if failed and 1 foe success
  */
 int main(void)
