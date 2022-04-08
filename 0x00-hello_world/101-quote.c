0b43061475a292a8b3514f7e742de62e6867b65e
1rubiz <1izekorruby@gmail.com>
2022-04-07 17:30:47 -0700

    advanced

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,11 @@
 #include <unistd.h>
 
 /*
- * main - print out a certain string followed by anew line.
+ * main- print out a certain string followed by anew line.
  * Return: 0 if failed and 1 foe success
  */
-int main(void){
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
