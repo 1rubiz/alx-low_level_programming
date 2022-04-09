3958a50eba215d26faf492529bf2cce3b1e42c66
1rubiz <1izekorruby@gmail.com>
2022-04-09 07:07:12 -0700

    new

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,9 +10,9 @@ int main(void)
 {
 	char az;
 
-	for (az = a; az <= z; az++)
+	for (az = 'a'; az <= 'z'; az++)
 		putchar(az);
-	for (az = A; az <= Z; az++)
+	for (az = 'A'; az <= 'Z'; az++)
 		putchar(az);
 	putchar('\n');
 	return(0);
