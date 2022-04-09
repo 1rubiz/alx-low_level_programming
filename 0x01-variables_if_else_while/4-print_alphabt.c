71ab8540546b73178066d79348230342b04a89c3
1rubiz <1izekorruby@gmail.com>
2022-04-09 07:18:27 -0700

    alphabets with no e or q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main- prints all alphabet in lower case
+ * except q and e.
+ * Return: 0 if successful.
+ */
+int main(void)
+{
+	char az = 'a';
+	while(az <= 'z')
+	{
+		if (az != 'e' || az != 'q')
+		{
+		       putchar(az);
+		}
+		az++
+	}
+	putchar('\n');
+	return (0);
+}
