20d294986003829510cbd9eac6a8ee74c5d9d7e2
1rubiz <1izekorruby@gmail.com>
2022-04-09 08:28:06 -0700

    1-9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main- prints 1-9.
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <=9; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
