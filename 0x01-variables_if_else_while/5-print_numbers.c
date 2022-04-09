1bc2b4a093f2b43cab1e5958d84ac247cfe0e303
1rubiz <1izekorruby@gmail.com>
2022-04-09 08:20:57 -0700

    1-9

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
- * main- prints 0-9 
- * Returns: o if successful
+ * main- prints 0-9
+ * Return: o if successful
  */
 
 int main(void)
 {
 	int b;
+
 	for (b = 0; b < 10; b++)
 		printf("%d", b);
 	printf("\n");
