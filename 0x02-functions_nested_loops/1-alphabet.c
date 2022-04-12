43805442797969f7ef7fd36f7f9b2aabdc518f04
1rubiz <1izekorruby@gmail.com>
2022-04-12 13:03:10 -0700

    functions and nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main- prints all alphabets in lower case
+ * Return: 0 if successful
+ */
+void print_alphabet(void);
+
+void print_alphabet()
+{
+	char x;
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main- prints the alphabets ten times on
+ * different lines followed by a new line
+ * Return: 0 if successful
+ */
+
+void print_alphabet_x10(void);
+
+void print_alphabet_x10()
+{
+	int x, y;
+	for (y = 0; y < 10; y++)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+		{
+			putchar(x);
+		}
+		putchar('\n');
+	}
+	printf("\n");
+	return (0);
+}
