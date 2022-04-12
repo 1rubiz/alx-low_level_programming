98721177db52708780f792ef8017424a97a307c3
1rubiz <1izekorruby@gmail.com>
2022-04-12 16:47:21 -0700

    digit signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * print_sign- prints + if n>0, prints
+ * 0 if n=0 and prints - if n <0.
+ * @n: represents the value.
+ *
+ * Return: 0 if successful
+ */
+int print_sign(int n);
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+		printf("+");
+	} else if (n == 0)
+	{
+		return (0);
+		printf("0");
+	} else if (n < 0)
+	{
+		return (-1);
+		printf("-");
+	}
+	return (0);
+}
