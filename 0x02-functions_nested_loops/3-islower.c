cb511c0b4bb058eb6bf4082792c12d15b45a376a
1rubiz <1izekorruby@gmail.com>
2022-04-12 15:06:01 -0700

    lower case function

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * _islower- returns 1 is the value is lower case
+ * and 0 if it is not.
+ */
+int _islower(int c);
+
+int _islower(int c)
+{
+	if (islower(c))
+	{
+		return (1);
+	} else {
+		return (0);
+	}
+}
