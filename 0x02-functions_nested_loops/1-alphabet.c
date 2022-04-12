0d55ed1a424cb497479a4ac30151d1d3eb6017ef
1rubiz <1izekorruby@gmail.com>
2022-04-12 13:46:19 -0700

    declare int

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,9 +4,9 @@
  * main- prints all alphabets in lower case
  * Return: 0 if successful
  */
-void print_alphabet(void);
+void print_alphabet();
 
-void print_alphabet(void)
+void print_alphabet()
 {
 	char x;
 	for (x = 'a'; x <= 'z'; x++)
