bf8e4f698f9a1debc1ed36b79fc3f09d9d6139c4
1rubiz <1izekorruby@gmail.com>
2022-04-12 17:00:07 -0700

    digit signs

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,16 +14,16 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
+		printf("+");
 		return (1);
-		printf("+1");
 	} else if (n == 0)
 	{
+		printf("0");
 		return (0);
-		printf("00");
 	} else if (n < 0)
 	{
+		printf("-");
 		return (-1);
-		printf("-1");
 	}
 	return (0);
 }
