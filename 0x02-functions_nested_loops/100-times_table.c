4cb6a20543be3cb4cbc6d590f64464d57b71d36b
1rubiz <1izekorruby@gmail.com>
2022-04-13 18:37:36 -0700

    time tables

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * print_times_table- prints a times table in
+ * response to argument given
+ *@n: parameter.
+ *
+ * Description: prints a times table.
+ *
+ * Return: returns void.
+ */
+
+void print_times_table(int n);
+
+void print_times_table(int n)
+{
+	int col, row, product;
+
+	if (n < 15 && n >= 0)
+	{
+		for (row = 0; row <= n; row++)
+		{
+			for (col = 0; col <=n; col++)
+			{
+				product = row * col;
+
+				if (col == 0)
+				{
+					_putchar('0');
+				} else if (product < 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(product % 10 + '0');
+				} else if (product >= 10 && product <100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((product / 10) % 10 + '0');
+					_putchar(product % 10 + '0');
+				} else if (product > 99 && product < 1000)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(product /100 + '0');
+					_putchar((product / 10) % 10 + '0');
+					_putchar(product % 10 + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
