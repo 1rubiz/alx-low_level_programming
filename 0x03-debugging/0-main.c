6a5b33ce93480a9f1de308bdd44aec9a1f205578
1rubiz <1izekorruby@gmail.com>
2022-04-14 10:42:19 -0700

    largest number function

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - prints if integer is positive or negative
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	int 1 = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}
