ef2a5e853cc952c3e46920a89f69540028a98305
1rubiz <1izekorruby@gmail.com>
2022-04-16 07:55:13 -0700

    buzzing

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,9 +10,9 @@
 
 int main(void)
 {
-	int x;
+	int x = 1;
 
-	for (x = 1; x <= 100; x++)
+	for (; x < 100; x++)
 	{
 		if (x % 3 == 0 && x % 5 == 0)
 			printf("FizzBuzz ");
@@ -23,7 +23,7 @@ int main(void)
 		else
 			printf("%d ", x);
 	}
-	printf("\n");
+	printf("Buzz\n");
 
 	return (0);
 }
