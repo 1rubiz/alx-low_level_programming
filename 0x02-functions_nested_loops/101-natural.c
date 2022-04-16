b254538038545df14f7c09f53e8a581fbdee79dd
1rubiz <1izekorruby@gmail.com>
2022-04-16 08:46:15 -0700

    multiple naturals

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the sum of the the multiple
+ * of 3 and 5 less than 1024.
+ * Return: 0 if successful.
+ */
+
+int main(void)
+{
+	unsigned long int x, y, z;
+	int a;
+
+	x = 0;
+	y = 0;
+	z = 0;
+
+	for (a = 0; a < 1024; ++a)
+	{
+		if ((a % 3) == 0)
+		{
+			x = x + a;
+		} else if ((a % 5) == 0)
+		{
+			y = y + a;
+		}
+	}
+	z = x + y;
+	printf("%lu\n", sum);
+	return (0);
+}
