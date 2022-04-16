619a057160300ca4becb6eed437bf3f93963fed8
1rubiz <1izekorruby@gmail.com>
2022-04-16 08:27:37 -0700

    kjdfnerrjrnrjrrr

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -21,31 +21,34 @@ void print_times_table(int n)
 	{
 		for (row = 0; row <= n; row++)
 		{
-			for (col = 0; col <=n; col++)
+			for (col = 0; col <= n; col++)
 			{
 				product = row * col;
 
 				if (col == 0)
 				{
 					_putchar('0');
-				} else if (product < 10)
+				}
+				else if (product < 10)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(product % 10 + '0');
-				} else if (product >= 10 && product <100)
+				}
+				else if (product >= 10 && product < 100)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar((product / 10) % 10 + '0');
 					_putchar(product % 10 + '0');
-				} else if (product > 99 && product < 1000)
+				}
+				else if (product > 99 && product < 1000)
 				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar(product /100 + '0');
+					_putchar(product / 100 + '0');
 					_putchar((product / 10) % 10 + '0');
 					_putchar(product % 10 + '0');
 				}
