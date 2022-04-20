67d5e94d3765e738244b06155bde8773e92acfc5
1rubiz <1izekorruby@gmail.com>
2022-04-20 11:11:08 -0700

    String reverse function

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_rev - prints the given string in reverse.
+ * @s: parameter.
+ *
+ * Return: void.
+ */
+
+void print_rev(char *s)
+{
+	printf("%s\n", strrev(s));
+}
