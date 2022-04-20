bc58b4be858ef76a617a56dc665d2ddabb46cb08
1rubiz <1izekorruby@gmail.com>
2022-04-20 10:44:08 -0700

    "Puts" funtion

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts - prints the strings along with a newline
+ * without the null value '\0'.
+ * @str: parameter
+ *
+ * Return: void.
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
