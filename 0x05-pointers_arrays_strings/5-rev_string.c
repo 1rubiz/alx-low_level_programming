695dc9a80b72f2ebaaf23a4c54e13c494c24dcc2
1rubiz <1izekorruby@gmail.com>
2022-04-20 12:24:21 -0700

    string reversal

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,5 +12,15 @@
 
 void rev_string(char *s)
 {
-	strrev(s);
+	int i;
+	int j = 0;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		j++;
+	}
+	for (i = j - 1; i >= 0; i++)
+	{
+		return (*(s + i));
+	}
 }
