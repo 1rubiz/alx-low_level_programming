c6faec30374313d25262d517707fb0ca18d3c4dd
1rubiz <1izekorruby@gmail.com>
2022-04-20 12:08:16 -0700

    string reversal

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: parameter.
+ *
+ * Return: void.
+ */
+
+void rev_string(char *s)
+{
+	strrev(s);
+}
