47adedc551be65cd413d62b82307246a030b5ea9
1rubiz <1izekorruby@gmail.com>
2022-04-20 11:55:23 -0700

    String reverse function

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,5 +12,15 @@
 
 void print_rev(char *s)
 {
-	printf("%s\n", strrev(s));
+	int i;
+	int j = 0;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		j++
+	}
+	for (i = j - 1; i >= 0; i--)
+	{
+		printf("%c", *(s + i));
+	}
 }
