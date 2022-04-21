d513e84c14243b88fd115d365b43b2ba111e0563
1rubiz <1izekorruby@gmail.com>
2022-04-21 08:56:06 -0700

    array printer

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -16,8 +16,12 @@ void print_array(int *a, int n)
 
 	for (x = 0; x <= n; x++)
 	{
-		printf("%d", *(a + 1));
-		if (x != (n - 1))
+		printf("%d", a[x]);
+		if (x == (n - 1))
+		{
+			putchar('\n');
+		}
+		else
 		{
 			putchar(',');
 		}
