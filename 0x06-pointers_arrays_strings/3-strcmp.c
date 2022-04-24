f278564b8c303d025d1a00bf75ef4e82e1c22f11
1rubiz <1izekorruby@gmail.com>
2022-04-23 17:11:26 -0700

    arrggggghhh

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,5 +10,27 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	strcmp(s1, s2);
+	int i, x;
+	long int length1 = 0, length2 = 0;
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		length1++;
+	}
+	for (x = 0; s2[x] != '\0'; x++)
+	{
+		length2++;
+	}
+
+	if (length1 > length2)
+	{
+		return (15);
+	} else if (length1 < length2)
+	{
+		return (-15);
+	}
+	else
+	{
+		return (0);
+	}
 }
--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,14 +11,11 @@ char *string_toupper(char *s)
 {
 	int i;
 
-	i = 0;
-
-	while (s[i] != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] >= 'a' && s[i] <= 'z')
 		{
 			s[i] = s[i] - 32;
 		}
-		i++;
 	}
 }
