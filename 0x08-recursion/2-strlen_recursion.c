8280d1ca7648ef0d0ee8cef0a65e66515e0fa127
1rubiz <1izekorruby@gmail.com>
2022-04-29 06:47:30 -0700

    recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - counts the length of string.
+ * @s: parameter
+ *
+ * Return: length of the string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int index = 0, l = 1;
+
+	if (s[index] != '\0')
+		return (l + _strlen_recursion(s + 1));
+}
