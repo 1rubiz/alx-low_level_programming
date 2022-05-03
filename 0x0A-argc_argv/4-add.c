bef8740234105f3625c48547e683b5b0976ab0eb
1rubiz <1izekorruby@gmail.com>
2022-05-03 15:35:25 -0700

    Argc and Argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,23 +11,27 @@
 
 int main(int argc, char *argv[])
 {
-	int a, j ,result = 0;
+	int a, j, result = 0;
+
 	for (a = 1; a < argc; a++)
-{
-         int value = atoi(argv[a]);
-	if (argc == 1)
 	{
-		putchar('0');
-                putchar('\n');
+		int value = atoi(argv[a]);
+
+		if (argc == 1)
+		{
+			putchar('0');
+			putchar('\n');
+		}
+		for (j = 0; argv[a][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[a][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		result += value;
 	}
-        for (j = 0; argv[a][j] != '\0'; j++)
-{
-	if (!isdigit(argv[a][j]))
-{		printf("Error\n");
-		return (1);
-}}
-         result += value;
-}
-         printf("%d\n", result);
+	printf("%d\n", result);
 	return (0);
 }
