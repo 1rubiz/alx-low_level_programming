60d7e59d332942b2f570206467ba1b95373456c2
1rubiz <1izekorruby@gmail.com>
2022-05-14 16:16:53 -0700

    object structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - object structure named dog.
+ * @name: parameter 1
+ * @age: parameter 2
+ * @owner: parameter 3
+ * Description: dogs, age and owner.
+ */
+
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+};
+#endif
