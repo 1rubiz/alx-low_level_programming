94c4cf446f8dda782dd3f6010f42bc189b2128ff
1rubiz <1izekorruby@gmail.com>
2022-05-26 15:40:00 -0700

    singly linked list in c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+include "lists.h"
+
+/**
+ * print_list - Prints all the available lists
+ * @h: parameter
+ *
+ * Return: the number of list.
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t value;
+
+	value = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[%d] (%s)\n", 0, "nil");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		value++;
+	}
+	return (value);
+}
