2406a53b1dbd2d98e0baa8318ca0387e65b816c3
1rubiz <1izekorruby@gmail.com>
2022-06-05 11:41:19 -0700

    This function creates a file and writes the desired content in to it

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,7 +28,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		text_content = "";
 	}
-	for (nletters = 0; text_content[nletters]; nletters++)
+	for (numletters = 0; text_content[numletters]; numletters++)
 		;
 
 	wr = write(fd, text_content, numletters);
