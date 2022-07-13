1fd9efd68dfb57787209738c76b000267b588485
1rubiz <1izekorruby@gmail.com>
2022-07-13 05:47:18 +0100

    dynamic library

--- 0x18-dynamic_libraries/_strcmp.c
+++ 0x18-dynamic_libraries/_strcmp.c
@@ -11,13 +11,9 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, x = 0;
-	while (x == 0)
-	{
-		for ((*(s1 + i) != '\0' &&) && (*(s2 + i) != '\0'))
-			break;
-		x = *(s1 + i) - *(s2 + i);
-		i++;
-	}
+
+        int x = 5;
+
+        printf("%s %s",s1,s2);
 	return (x);
 }
