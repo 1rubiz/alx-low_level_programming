9d4ab1551a77bdd40215647c8582a6aaf2b53b6e
Ruby Izekor <1izekorruby@gmail.com>
2022-07-12 20:37:24 -0700

    strlen.c

---
+++ 0x18-dynamic_libraries/_strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of the passed string
+ * @s: parameter
+ *
+ * Return: the length of string.
+ */
+
+int _strlen(char *s)
+{
+	int lenn = strlen(s);
+
+	return (lenn);
+}
