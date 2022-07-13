c295e938f5a5bbe3f427d52e06332adaf2aacc00
Ruby Izekor <1izekorruby@gmail.com>
2022-07-13 04:59:24 +0100

    Create _strcmp.c

---
+++ 0x18-dynamic_libraries/_strcmp.c
@@ -0,0 +1,23 @@
+
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings.
+ * @s1: parameter 1
+ * @s2: parameter 2.
+ *
+ * Return: void
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0, x = 0;
+	while (x == 0)
+	{
+		for ((*(s1 + i) != '\0' &&) && (*(s2 + i) != '\0'))
+			break;
+		x = *(s1 + i) - *(s2 + i);
+		i++;
+	}
+	return (x);
+}
