c4f83a67100de459fdfb1940ec1de1c2d4a7c10f
1rubiz <1izekorruby@gmail.com>
2022-07-28 15:38:20 -0700

    Indexing function

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,17 +1,17 @@
 #include "hash_tables.h"
 
-/*
-* key_index - gets the index of the key from the hash table
-* @key: key parameter
-* @size: size of table parameter
-*
-* Return: the index of the key
-*/
+/**
+ * key_index - gets the index of the key from the hash table
+ * @key: parameter for the key given
+ * @size: size of the array of the hash table
+ *
+ * Return: the index of the key
+ */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-   unsigned long int hash;
-   
-   hash = hash_djb2(key)
-   return (hush % size)
+	unsigned long int hash;
+
+	hash = hash_djb2(key);
+	return (hash % size);
 }
